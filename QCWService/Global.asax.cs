a22246d3e70d151a54a6109f4dd54f541a342923
Ellis <qianchenchn@foxmail.com>
2017-06-27 17:23:42 +0800

    HttpMethod == "OPTIONS"

--- QCWService/Global.asax.cs
+++ QCWService/Global.asax.cs
@@ -28,6 +28,12 @@ namespace QCWService
 
         protected void Session_Start(object sender, EventArgs e)
         {
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.End();
+            }
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
